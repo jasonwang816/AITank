4e0d0ccf3b8b539dfddccb9e5318e238f4a7e342
jason <jason@jasons-Mac.local>
2012-10-17 14:42:53 -0700

    Initial Commit

---
+++ AITank/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AITank
+//
+//  Created by jason on 12-10-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AITank/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AITank
+//
+//  Created by jason on 12-10-17.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
